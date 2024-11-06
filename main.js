3d74ca45e755c24d343163e0449e2fc24ea1a53b
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-06 16:49:28 +0100

    add css

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 //console.log("js ok");
 const emails =document.getElementById("list")
-
+const generateListBtn =document.getElementById("add-list")
 
 for(let i =0; i < 10 ; i++){
     fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
     .then((response) => response.json() )
     .then((data) => {
         const singleEmail = data.response;
-        emails.innerHTML += `<li>${singleEmail}</li>`;
-        console.log(emails);
+        emails.innerHTML += `<a href="#" class=" list-group-item list-group-item-action text-center">${singleEmail}</a>`;
         
     })
     
 }
+
+
+//function generateNewEmailList (num){
+    //for(let i =0; i < num ; i++){
+       // const singleEmail="";
+        //fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        //.then((response) => response.json() )
+        //.then((data) => {
+             //singleEmail = data.response;
+        
+        //})  
+    //}
+    
+//}
+
+//let esempio = generateNewEmailList(5);
+//console.log(esempio);
