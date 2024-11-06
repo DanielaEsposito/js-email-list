6a0734397d509f245f5bc0eeb1dbbf3893a9b89d
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-06 14:44:17 +0100

    add list emails in document

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-console.log("js ok");
+//console.log("js ok");
+const emails =document.getElementById("list")
+
 
 for(let i =0; i < 10 ; i++){
-
     fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
     .then((response) => response.json() )
     .then((data) => {
         const singleEmail = data.response;
+        emails.innerHTML += `<li>${singleEmail}</li>`;
+        console.log(emails);
         
-        console.log(singleEmail);
-        
-    });
+    })
+    
 }
